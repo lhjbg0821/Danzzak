05b5e199899de5da18aee45a33d1d955e1b41b14
ollok_99 <lhjbg0821@naver.com>
2022-10-12 14:40:26 +0900

    third commit

--- passport/index.js
+++
@@ -1,7 +0,0 @@
-const passport = require('passpoart');
-const KakaoStrategy = require('passport-kakao').Strategy;
-const { User } = require('../models')
-
-module.exports = (app) => {
-    app.use(passport.initialize()); //
-}
